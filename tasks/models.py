0a0ad01ddc73674be9667c1f153e586285af8cb7
egorpon <ponomarenkoegor36@gmail.com>
2026-05-26 14:46:49 +0300

    feat: add due_date validation for Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from todos.models import TodoList
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 # Create your models here.
@@ -9,9 +11,13 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField(blank=True, null=True)
-    is_completed = models.BooleanField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
 
     todo = models.ForeignKey(TodoList, on_delete=models.CASCADE, related_name="tasks")
 
     def __str__(self):
         return self.name
+
+    def clean(self):
+        if timezone.now() >= self.due_date:
+            raise ValidationError("Due date cannot be in the past")
