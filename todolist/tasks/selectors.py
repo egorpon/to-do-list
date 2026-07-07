e808cfc7043ea944bec450459957b7d2be7865cd
egorpon <ponomarenkoegor36@gmail.com>
2026-07-07 16:25:31 +0300

    feat(tasks): add daily summary email template

--- todolist/tasks/selectors.py
+++ todolist/tasks/selectors.py
@@ -32,23 +32,34 @@ def get_tasks_stats(*, user: User) -> dict:
     )
 
 
-def get_overdue_tasks(*, user: User) -> QuerySet[Task]:
+def get_overdue_tasks(*, user: User, limit: int = 5) -> QuerySet[Task]:
     today = timezone.now()
 
-    return Task.objects.filter(todo__owner=user, due_date__lt=today, is_completed=False)
+    return Task.objects.filter(
+        todo__owner=user, due_date__lt=today, is_completed=False
+    ).order_by("-due_date")[:limit]
 
 
-def get_upcoming_tasks_this_week(*, user: User) -> QuerySet[Task]:
-    today = timezone.now()
+def get_upcoming_tasks_this_week(*, user: User, limit: int = 5) -> QuerySet[Task]:
+    today = timezone.localdate()
     week_start = today - datetime.timedelta(days=today.weekday())
     week_end = week_start + datetime.timezone(days=6)
 
     return Task.objects.filter(
         todo__owner=user,
-        due_date__gte=week_start,
-        due_date__lte=week_end,
+        due_date__date__gte=week_start,
+        due_date__date__lte=week_end,
         is_completed=False,
-    )
+    ).order_by("-due_date")[:limit]
+
+
+def get_completed_tasks_this_week(*, user: User, limit: int = 5) -> QuerySet[Task]:
+    today = timezone.localdate()
+    week_start = today - datetime.timedelta(days=today.weekday())
+
+    return Task.objects.filter(
+        todo__owner=user, is_completed=True, completed_at__date__gte=week_start
+    ).order_by("-completed_at")[:limit]
 
 
 def get_users_with_active_tasks() -> QuerySet[User]:
